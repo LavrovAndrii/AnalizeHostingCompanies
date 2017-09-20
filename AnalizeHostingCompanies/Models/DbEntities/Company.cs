36f4c6f9dbdc15fad91d595b94640940b66d9b13
Andrii Lavrov <andriy.lavrov@gmail.com>
2017-09-21 02:39:38 +0300

    Add models

---
+++ AnalizeHostingCompanies/Models/DbEntities/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Policy;
+using System.Web;
+
+namespace AnalizeHostingCompanies.Models.DbEntities
+{
+    public class Company
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string Site { get; set; }
+        public string InternetProviderId { get; set; }
+        public string ServerDataCenterId { get; set; }
+        //public int Rating  { get; set; }
+
+    }
+}
