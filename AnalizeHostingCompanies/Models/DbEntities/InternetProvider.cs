24680889902f232145a4eae25d4cad8b0cd2a48d
Andrii Lavrov <andriy.lavrov@gmail.com>
2017-09-21 02:26:27 +0300

    Add models

---
+++ AnalizeHostingCompanies/Models/DbEntities/InternetProvider.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnalizeHostingCompanies.Models.DbEntities
+{
+    public class InternetProvider
+    {
+        [Key]
+
+        public string Name { get; set; }
+
+        public int SpeedConnection { get; set; }
+        public int Traffic { get; set; }
+        public string PoolIpAddress { get; set; }
+
+
+    }
+}
